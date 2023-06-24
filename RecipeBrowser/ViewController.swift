7576e95e97322afff15cb2e33171ae6fd59aede3
Yemi Ajibola <yajibola23@gmail.com>
2023-06-23 23:14:24 -0500

    Initial Commit

---
+++ RecipeBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeBrowser
+//
+//  Created by Yemi Ajibola on 6/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
