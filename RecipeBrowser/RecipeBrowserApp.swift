9452c7005a3d6af5f793ddb7672c3c13b9d5c090
Yemi <yajibola23Agmail.clom>
2025-02-17 11:15:25 -0600

    Initial Commit

---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Amira Ajibola  on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
