43b85bec481dcd0b95ea67fc7a07cf62ec95c69b
Yemi <yajibola23Agmail.clom>
2025-02-17 11:15:25 -0600

    Initial Commit

---
+++ RecipeBrowserTests/RecipeBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserTests.swift
+//  RecipeBrowserTests
+//
+//  Created by Amira Ajibola  on 2/17/25.
+//
+
+import Testing
+@testable import RecipeBrowser
+
+struct RecipeBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
