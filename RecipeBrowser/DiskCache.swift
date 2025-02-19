26b2fa01111d0b7c90a8f5024f80105f31a37875
Yemi <yajibola23Agmail.clom>
2025-02-18 18:59:35 -0600

    Moved `DiskCache` to production.

---
+++ RecipeBrowser/DiskCache.swift
@@ -0,0 +1,48 @@
+//
+//  DiskCache.swift
+//  RecipeBrowser
+//
+//  Created by Amira Ajibola  on 2/18/25.
+//
+
+import UIKit
+
+public final class DiskCache {
+    
+    private let fileManager = FileManager.default
+    private var cacheDirectory: URL
+    
+    init() {
+        let cachesFolder = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        cacheDirectory = cachesFolder.appending(path: "ImageCache", directoryHint: .isDirectory)
+        
+        if !fileManager.fileExists(atPath: cacheDirectory.path()) {
+            try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
+        }
+    }
+    
+    private func cachePath(for url: URL) -> URL {
+        let fileName = url.lastPathComponent
+        return cacheDirectory.appending(component: fileName)
+    }
+    
+    func saveImage(_ image: UIImage, for url: URL) async {
+        guard let imageData = image.pngData() else { return }
+        try? imageData.write(to: cachePath(for: url))
+    }
+    
+    func loadImage(for url: URL) async -> UIImage? {
+        let path = cachePath(for: url)
+        
+        return UIImage(contentsOfFile: path.path())
+    }
+    
+    func containsImage(for url: URL) -> Bool {
+        return fileManager.fileExists(atPath: cachePath(for: url).path())
+    }
+    
+    func clearCache() {
+        try? fileManager.removeItem(at: cacheDirectory)
+        try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
+    }
+}
--- RecipeBrowserTests/DiskCacheTests.swift
+++ RecipeBrowserTests/DiskCacheTests.swift
@@ -6,46 +6,7 @@
 //
 
 import XCTest
-
-class DiskCache {
-    
-    private let fileManager = FileManager.default
-    private var cacheDirectory: URL
-    
-    init() {
-        let cachesFolder = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        cacheDirectory = cachesFolder.appending(path: "ImageCache", directoryHint: .isDirectory)
-        
-        if !fileManager.fileExists(atPath: cacheDirectory.path()) {
-            try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
-        }
-    }
-    
-    private func cachePath(for url: URL) -> URL {
-        let fileName = url.lastPathComponent
-        return cacheDirectory.appending(component: fileName)
-    }
-    
-    func saveImage(_ image: UIImage, for url: URL) async {
-        guard let imageData = image.pngData() else { return }
-        try? imageData.write(to: cachePath(for: url))
-    }
-    
-    func loadImage(for url: URL) async -> UIImage? {
-        let path = cachePath(for: url)
-        
-        return UIImage(contentsOfFile: path.path())
-    }
-    
-    func containsImage(for url: URL) -> Bool {
-        return fileManager.fileExists(atPath: cachePath(for: url).path())
-    }
-    
-    func clearCache() {
-        try? fileManager.removeItem(at: cacheDirectory)
-        try? fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
-    }
-}
+@testable import RecipeBrowser
 
 final class DiskCacheTests: XCTestCase {
     var sut: DiskCache!
