0bd0f5b0d3a5c5a857a94ca89b78034915a8b5ca
Yemi <yajibola23gmail.com>
2025-02-19 13:08:54 -0600

    Moved `ImageCacheManager` to production.

--- RecipeBrowserTests/ImageCacheManagerTests.swift
+++ RecipeBrowserTests/ImageCacheManagerTests.swift
@@ -9,41 +9,6 @@ import XCTest
 import Foundation
 @testable import RecipeBrowser
 
-class ImageCacheManager {
-    private let diskCache: ImageCachable
-    private let memoryCache: ImageCachable
-    private let downloader: ImageDownloadable
-    
-    init(diskCache: ImageCachable, memoryCache: ImageCachable, downloader: ImageDownloadable) {
-        self.diskCache = diskCache
-        self.memoryCache = memoryCache
-        self.downloader = downloader
-    }
-    
-    func loadImage(from url: URL) async throws -> UIImage? {
-        if let memoryImage = memoryCache.loadImage(for: url) {
-            return memoryImage
-        } else if let diskImage = diskCache.loadImage(for: url) {
-            return diskImage
-        } else {
-            do {
-                let image = try await downloader.fetchImage(from: url)
-                if let downloadedImage = image {
-                    memoryCache.saveImage(downloadedImage, for: url)
-                    diskCache.saveImage(downloadedImage, for: url)
-                }
-                
-                return image
-            }
-        }
-    }
-    
-    func clearCache() {
-        memoryCache.clearCache()
-        diskCache.clearCache()
-    }
-}
-
 final class ImageCacheManagerTests: XCTestCase {
     fileprivate var memoryCache: MockInMemoryCache?
     fileprivate var diskCache: MockDiskCache?
