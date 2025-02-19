1a63066dcde9e5a6e6fdd3ab05e8f9ef8ac08cbc
Yemi <yajibola23gmail.com>
2025-02-19 00:22:36 -0600

    Moved `ImageDownloader`  to production

--- RecipeBrowserTests/ImageDownloaderTests.swift
+++ RecipeBrowserTests/ImageDownloaderTests.swift
@@ -6,49 +6,7 @@
 //
 
 import XCTest
-
-class ImageDownloader {
-    enum Error: Swift.Error {
-        case invalidURL
-        case networkFailure(statusCode: Int)
-        case imageDecodeFailure
-        case unknown(Swift.Error)
-    }
-    
-    let session: URLSession
-    
-    init(session: URLSession) {
-        self.session = session
-    }
-    
-    func fetchImage(from url: URL) async throws(Error) -> UIImage? {
-        guard url.scheme == "http" || url.scheme == "https" else {
-            throw Error.invalidURL
-        }
-        
-        do {
-            let (data, response) = try await session.data(from: url)
-            
-            guard let httpUrlResponse = response as? HTTPURLResponse else {
-                throw Error.networkFailure(statusCode: 0)
-            }
-            
-            guard httpUrlResponse.statusCode == 200 else {
-                throw Error.networkFailure(statusCode: httpUrlResponse.statusCode)
-            }
-            
-            guard let image = UIImage(data: data) else {
-                throw Error.imageDecodeFailure
-            }
-            return image
-        } catch let error as Error {
-            throw error
-        } catch {
-            throw .unknown(error)
-        }
-    }
-}
-
+@testable import RecipeBrowser
 
 final class ImageDownloaderTests: XCTestCase {
     func testImageDownloaderFailsWithNetworkFailure() async {
