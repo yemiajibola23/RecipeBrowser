72822f2bdf251a976312bb673c2bf36a6a78b301
Yemi <yajibola23gmail.com>
2025-02-19 13:08:54 -0600

    Moved `ImageCacheManager` to production.

---
+++ RecipeBrowser/ImageCacheManager.swift
@@ -0,0 +1,43 @@
+//
+//  ImageCacheManager.swift
+//  RecipeBrowser
+//
+//  Created by Amira Ajibola  on 2/19/25.
+//
+
+import UIKit
+
+class ImageCacheManager {
+    private let diskCache: ImageCachable
+    private let memoryCache: ImageCachable
+    private let downloader: ImageDownloadable
+    
+    init(diskCache: ImageCachable, memoryCache: ImageCachable, downloader: ImageDownloadable) {
+        self.diskCache = diskCache
+        self.memoryCache = memoryCache
+        self.downloader = downloader
+    }
+    
+    func loadImage(from url: URL) async throws -> UIImage? {
+        if let memoryImage = memoryCache.loadImage(for: url) {
+            return memoryImage
+        } else if let diskImage = diskCache.loadImage(for: url) {
+            return diskImage
+        } else {
+            do {
+                let image = try await downloader.fetchImage(from: url)
+                if let downloadedImage = image {
+                    memoryCache.saveImage(downloadedImage, for: url)
+                    diskCache.saveImage(downloadedImage, for: url)
+                }
+                
+                return image
+            }
+        }
+    }
+    
+    func clearCache() {
+        memoryCache.clearCache()
+        diskCache.clearCache()
+    }
+}
