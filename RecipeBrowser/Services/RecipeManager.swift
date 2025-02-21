70e53af13e212cf9eedc05c774d748cd0252d4a9
Yemi <yajibola23gmail.com>
2025-02-20 22:21:35 -0600

    Moved `RecipeManager` to production.

---
+++ RecipeBrowser/Services/RecipeManager.swift
@@ -0,0 +1,48 @@
+//
+//  RecipeManager.swift
+//  RecipeBrowser
+//
+//  Created by Amira Ajibola  on 2/20/25.
+//
+
+import Foundation
+
+final class RecipeManager {
+    enum Error: Swift.Error {
+        case network(NetworkService.Error)
+        case decoding(Swift.Error)
+        case unknown(Swift.Error)
+    }
+    
+    let networkService: NetworkServiceProtocol
+    
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+    
+    func fetchRecipes(from url: URL) async throws(Error) -> [Recipe] {
+        do {
+            let data = try await networkService.handleRequest(for: url)
+            let root = try JSONDecoder().decode(RecipeRepository.self, from: data)
+            return root.recipes
+        } catch let networkError as NetworkService.Error {
+            throw .network(networkError)
+        } catch let decodingError as DecodingError {
+            throw .decoding(decodingError)
+        } catch {
+            print(error.localizedDescription)
+            throw .unknown(error)
+        }
+    }
+}
+
+extension RecipeManager.Error: Equatable {
+    static func ==(lhs: RecipeManager.Error, rhs: RecipeManager.Error) -> Bool {
+        switch (lhs, rhs) {
+        case let (.network(lError), .network(rError)): return lError == rError
+        case (.decoding, .decoding): return true
+        case let (.unknown(lError), .unknown(rError)): return lError.localizedDescription ==  rError.localizedDescription
+        default: return false
+        }
+    }
+}
--- RecipeBrowserTests/RecipeManagerTests.swift
+++ RecipeBrowserTests/RecipeManagerTests.swift
@@ -8,46 +8,6 @@
 import XCTest
 @testable import RecipeBrowser
 
-class RecipeManager {
-    enum Error: Swift.Error {
-        case network(NetworkService.Error)
-        case decoding(Swift.Error)
-        case unknown(Swift.Error)
-    }
-    
-    let networkService: NetworkServiceProtocol
-    
-    init(networkService: NetworkServiceProtocol) {
-        self.networkService = networkService
-    }
-    
-    func fetchRecipes(from url: URL) async throws(Error) -> [Recipe] {
-        do {
-            let data = try await networkService.handleRequest(for: url)
-            let root = try JSONDecoder().decode(RecipeRepository.self, from: data)
-            return root.recipes
-        } catch let networkError as NetworkService.Error {
-            throw Error.network(networkError)
-        } catch let decodingError as DecodingError {
-            throw .decoding(decodingError)
-        }catch {
-            print(error.localizedDescription)
-            throw .unknown(error)
-        }
-    }
-}
-
-extension RecipeManager.Error: Equatable {
-    static func ==(lhs: RecipeManager.Error, rhs: RecipeManager.Error) -> Bool {
-        switch (lhs, rhs) {
-        case let (.network(lError), .network(rError)): return lError == rError
-        case (.decoding, .decoding): return true
-        case let (.unknown(lError), .unknown(rError)): return lError.localizedDescription ==  rError.localizedDescription
-        default: return false
-        }
-    }
-}
-
 final class RecipeManagerTests: XCTestCase {
     func testRecipeManagerFetchRecipesSucceedsReturnsRecipes() async {
         // Given
